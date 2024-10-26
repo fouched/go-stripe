a6adf63579a878a95600485382edf077155906c0
Fouche du Preez <fouche@limehouse.co.za>
2024-10-26 10:37:53 +0200

    init

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -54,7 +54,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment {development|production}")
-	flag.StringVar(&cfg.api, "api", "http://localhost:4001", "URL to API")
+	flag.StringVar(&cfg.api, "api", "http://localhost:4001/api/payment-intent", "URL to API")
 
 	flag.Parse()
 
